0921336d6ce4b85983a8f404da1ba8378d95cf76
vamshi7589 <vamshi7589@gmail.com>
2020-11-03 14:52:37 +0530

    added: random flips program added

--- flipCoinSimulation.sh
+++ flipCoinSimulation.sh
@@ -1,1 +1,20 @@
 echo "*********welcome to flip coin simulation***********"
+# Hello World Program in Bash Shell
+
+#!/bin/bash
+echo "Welcome to flip coin simulator"
+isTie=0
+hcount=0
+tcount=0
+diff=99
+res=""
+
+function headTailGenerate(){
+	rn=$((RANDOM%2))
+       	if [ $rn -eq 0 ]
+       	then
+              	tcount=$(($tcount+1))
+       	else
+               	hcount=$(($hcount+1))
+       	fi
+}
